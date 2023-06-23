c9143b8a49df22824609df70673ce07825a8b5f0
Diary-hub <diary.313205014@uhd.edu.iq>
2023-06-23 04:02:02 +0300

    LangChain Librarys

--- Backend/app.py
+++ Backend/app.py
@@ -1,26 +1,29 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
+from functions import *
 import sys
 import os
 
-template_dir = os.path.abspath("FrontEnd/dist")
-app = Flask(__name__, template_folder=template_dir)
+
+template_dir = os.path.abspath("FrontEnd/pages")
+static_dir = os.path.abspath("FrontEnd/src")
+app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 CORS(app)
 
 
 @app.get("/")
 def index_get():
-    return render_template("index.html")
+    return render_template("loginPage.html")
 
 
-# @app.route("/WhoIsIt", methods=["GET", "POST"])
-# def getfaces():
-# text = request.get_json()
-# response = Recognize(text["message"])
-# reply = {"answer": response}
-# return jsonify(reply)
-# os.getenv('GCP_PROJECT_ID')
+@app.route("/WhoIsIt", methods=["GET", "POST"])
+def getfaces():
+    text = request.get_json()
+    response = Recognize(text["message"])
+    reply = {"answer": response}
+    return jsonify(reply)
+    os.getenv("GCP_PROJECT_ID")
 
 
 if __name__ == "__main__":
---
+++ Backend/functions.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+from htmlTemplates import css, bot_template, user_template
+
+
+def getChunksOfText(raw_texts):
+    text_splitter = CharacterTextSplitter(
+        separator="\n", chunk_size=1000, chunk_overlap=200, length_function=len
+    )
+    chunks = text_splitter.split_text(raw_texts)
+    return chunks
