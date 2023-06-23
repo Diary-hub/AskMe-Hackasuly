36a15cce4054d74d2df867666122842bbb89e9e6
Kruuzz <karzan2000barzan@gmail.com>
2023-06-23 12:15:16 +0300

    authentication for login

--- application/askme/lib/login.dart
+++ application/askme/lib/login.dart
@@ -1,18 +1,43 @@
 // ignore_for_file: avoid_unnecessary_containers
 
+import 'package:askme/my_button.dart';
+import 'package:askme/signup.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Login_page extends StatefulWidget {
-  const Login_page({super.key});
+  Login_page({super.key});
+  //controller
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+// sign user in method
+  void signUserIn() async {
+    await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text, password: passwordController.text);
+
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: emailController.text, password: passwordController.text);
+    } on FirebaseException catch (e) {
+      if (e.code == 'user-not-found') {
+        print("no");
+      } else if (e.code == 'wrong-password') {
+        print('wrong pass');
+      }
+    }
+  }
 
   @override
   State<Login_page> createState() => _Login_pageState();
 }
 
 class _Login_pageState extends State<Login_page> {
+  get signUserIn => null;
+
   @override
   Widget build(BuildContext context) {
-  //  double screenwidth = MediaQuery.of(context).size.width;
+    //  double screenwidth = MediaQuery.of(context).size.width;
 
     return SafeArea(
       child: Scaffold(
@@ -61,15 +86,15 @@ class _Login_pageState extends State<Login_page> {
                         decoration: const InputDecoration(
                           filled: true,
                           fillColor: Colors.transparent,
-                          hintText:
+                          labelText:
                               ("                                      ئیمەیڵەکەت داخڵ بکە "),
+                          floatingLabelBehavior: FloatingLabelBehavior.auto,
                           enabledBorder: UnderlineInputBorder(
                             borderSide: BorderSide(
                               color: Color(0xFF119C59),
                             ), // Set your desired color here
                           ),
-                          labelStyle: TextStyle(
-                              color: Color.fromARGB(255, 28, 127, 252)),
+                          labelStyle: TextStyle(color: Color(0xFF52575C)),
                         ),
                       ),
                     ),
@@ -84,60 +109,22 @@ class _Login_pageState extends State<Login_page> {
                         decoration: const InputDecoration(
                           filled: true,
                           fillColor: Colors.transparent,
-                          hintText:
+                          labelText:
                               ("                                      پاسۆدەکەت داخڵ بکە"),
+                          floatingLabelBehavior: FloatingLabelBehavior.auto,
                           enabledBorder: UnderlineInputBorder(
                             borderSide: BorderSide(
                               color: Color(0xFF119C59),
                             ), // Set your desired color here
                           ),
-                          labelStyle: TextStyle(
-                              color: Color.fromARGB(255, 28, 127, 252)),
+                          labelStyle: TextStyle(color: Color(0xFF52575C)),
                         ),
                       ),
                     ),
                   ),
                   // login button
-                  Padding(
-                    padding: const EdgeInsets.only(top: 100),
-                    child: Center(
-                      child: SizedBox(
-                        width: 210,
-                        height: 51,
-                        child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(25),
-                              ),
-                              backgroundColor:
-                                  const Color.fromARGB(255, 236, 236, 236),
-                            ),
-                            onPressed: () {
-                              // Navigator.push(
-                              //   context,
-                              //   MaterialPageRoute(
-                              //       builder: (context) => null),
-                              // );
-                            },
-                            child: const Padding(
-                              padding: EdgeInsets.only(top: 9),
-                              child: Column(
-                                children: [
-                                  Center(
-                                    child: Text(
-                                      'چوونە ژورەوە',
-                                      style: TextStyle(
-                                          color: Color.fromARGB(255, 0, 0, 0),
-                                          fontSize: 20),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            )),
-
-                        // sign up
-                      ),
-                    ),
+                  My_Button(
+                    onTap: signUserIn,
                   ),
                 ],
               ),
@@ -147,13 +134,14 @@ class _Login_pageState extends State<Login_page> {
               padding: const EdgeInsets.only(top: 50),
               child: InkWell(
                   onTap: () {
-                    // Navigator.pushReplacement(
-                    //   context,
-                    //   MaterialPageRoute(builder: (context) => const AboutUs()),
-                    // );
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const SignUp_Page()),
+                    );
                   },
                   child: const Text(
-                    " دروست کردنی ئەکاونت",
+                    " دروستکردنی هەژمار",
                     style: TextStyle(
                         decoration: TextDecoration.underline,
                         fontSize: 18,
