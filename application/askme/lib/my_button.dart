0659c47d3246fd9290ff4f6af02f8c8ff485330a
Kruuzz <karzan2000barzan@gmail.com>
2023-06-23 17:48:34 +0300

    signup authentication done

--- application/askme/lib/my_button.dart
+++ application/askme/lib/my_button.dart
@@ -10,39 +10,42 @@ class My_Button extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: onTap,
-      child: Container(
-        padding: const EdgeInsets.all(25),
-        margin: const EdgeInsets.symmetric(horizontal: 25),
-        decoration: BoxDecoration(
-            color: Colors.black, borderRadius: BorderRadius.circular(20)),
-        child: Center(
-          child: Column(
-            children: [
-              Center(
-                child: InkWell(
-                  onTap: () {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (context) => Test()),
-                    );
-                  },
-                  child: const Text(
-                    'چوونە ژورەوە',
-                    style: TextStyle(
-                      decoration: TextDecoration.underline,
-                      fontSize: 18,
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      fontWeight: FontWeight.bold,
+      child: Padding(
+        padding: const EdgeInsets.only(top: 50),
+        child: Container(
+          padding: const EdgeInsets.all(15),
+          margin: const EdgeInsets.symmetric(horizontal: 115),
+          decoration: BoxDecoration(
+              color: Color(0xFF119C59),
+              borderRadius: BorderRadius.circular(20)),
+          child: Center(
+            child: Column(
+              children: [
+                Center(
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => const Test()),
+                      );
+                    },
+                    child: const Text(
+                      'چوونە ژورەوە',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Color.fromARGB(255, 255, 255, 255),
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
+                  // child: Text(
+                  //   'چوونە ژورەوە',
+                  //   style: TextStyle(
+                  //       color: Color.fromARGB(255, 255, 255, 255), fontSize: 20),
+                  // ),
                 ),
-                // child: Text(
-                //   'چوونە ژورەوە',
-                //   style: TextStyle(
-                //       color: Color.fromARGB(255, 255, 255, 255), fontSize: 20),
-                // ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
--- application/askme/lib/signup.dart
+++ application/askme/lib/signup.dart
@@ -1,284 +1,242 @@
 import 'package:askme/login.dart';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class SignUp_Page extends StatefulWidget {
-  const SignUp_Page({Key? key}) : super(key: key);
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({Key? key}) : super(key: key);
 
   @override
-  State<SignUp_Page> createState() => _SignUp_PageState();
+  State<SignUpPage> createState() => _SignUpPageState();
 }
 
-class _SignUp_PageState extends State<SignUp_Page> {
+class _SignUpPageState extends State<SignUpPage> {
   final TextEditingController usernameController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
 
-  String? usernameErrorText;
-  String? emailErrorText;
-  String? passwordErrorText;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  void signUserIn() async {
+    try {
+      if (passwordController.text == confirmPasswordController.text) {
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: emailController.text,
+          password: passwordController.text,
+        );
+        // Account created successfully, you can perform any additional actions here
+      } else {
+        displaySnackBar(' پاسۆردەکەت دەبێت یەکسان بێت');
+      }
+    } catch (e) {
+      String errorMessage = 'هەڵەیەک ڕوویدا، تکایە دوبارە هەوڵبدەرەوە';
+      if (e is FirebaseAuthException) {
+        switch (e.code) {
+          case 'email-already-in-use':
+            errorMessage = 'ئەم ئیمەیڵە بەکارهێناوەتەوە لە سەر هەژمارێکی تر';
+            break;
+          case 'invalid-email':
+            errorMessage = 'ئیمەیڵەکە نادروستە';
+            break;
+          case 'weak-password':
+            errorMessage = 'وشەی نهێنی ناسنەکەوتە';
+            break;
+        }
+      }
+      displaySnackBar(errorMessage);
+    }
+  }
+
+  void displaySnackBar(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
 
   bool validateForm() {
-    bool isValid = true;
-
-    if (usernameController.text.isEmpty) {
-      setState(() {
-        usernameErrorText = 'تکایە ناوەکەت تۆماربکە';
-      });
-      isValid = false;
-    } else {
-      setState(() {
-        usernameErrorText = null;
-      });
-    }
-
-    if (emailController.text.isEmpty) {
-      setState(() {
-        emailErrorText = 'تکایە ئیمەیڵەکەت تۆماربکە';
-      });
-      isValid = false;
-    } else {
-      setState(() {
-        emailErrorText = null;
-      });
-    }
-
-    if (passwordController.text.isEmpty) {
-      setState(() {
-        passwordErrorText = 'تکایە وشەی نهێنی تۆمار بکە';
-      });
-      isValid = false;
-    } else if (passwordController.text.length < 8 ||
-        passwordController.text.length > 16) {
-      setState(() {
-        passwordErrorText = 'وشەی نهێنی پێویستە لە نێوان ٨ و ١٦ کارەکتەربێت';
-      });
-      isValid = false;
-    } else {
-      setState(() {
-        passwordErrorText = null;
-      });
-    }
-
+    bool isValid = _formKey.currentState?.validate() ?? false;
     return isValid;
   }
 
   void signUp() {
     if (validateForm()) {
-      // Perform signup logic here
-      // If all fields are valid, proceed with signup process
-      // Otherwise, the error messages will be displayed
-      // and the user won't be able to proceed until the fields are fixed
+      signUserIn();
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        resizeToAvoidBottomInset: false,
-        backgroundColor: const Color(0xFFFAF9F7),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              // ASKE ME
-              Container(
-                child: const Center(
-                  child: Padding(
-                    padding: EdgeInsets.only(top: 40),
-                    child: Text(
-                      "لێم بپرسە",
-                      style: TextStyle(
-                        color: Color(0xFF119C59),
-                        fontSize: 48,
-                        fontFamily: 'Montserrat',
+      child: Center(
+        child: Scaffold(
+          resizeToAvoidBottomInset: false,
+          backgroundColor: const Color(0xFFFAF9F7),
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  child: const Center(
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 40),
+                      child: Text(
+                        "لێم بپرسە",
+                        style: TextStyle(
+                          color: Color(0xFF119C59),
+                          fontSize: 48,
+                          fontFamily: 'Montserrat',
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              // login
-              Container(
-                child: const Center(
-                  child: Padding(
-                    padding: EdgeInsets.only(top: 40),
-                    child: Text(
-                      " دروستکردنی هەژمار ",
-                      style: TextStyle(
-                        color: Color(0xFF25282B),
-                        fontSize: 32,
-                        fontFamily: 'Montserrat',
+                Container(
+                  child: const Center(
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 40),
+                      child: Text(
+                        " دروستکردنی هەژمار",
+                        style: TextStyle(
+                          color: Color(0xFF119C59),
+                          fontSize: 30,
+                          fontFamily: 'Montserrat',
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-
-              // input fields
-              Container(
-                child: Column(
-                  children: [
-                    // username
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20, right: 30),
-                      child: SizedBox(
-                        width: 272,
-                        height: 55,
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          top: 50,
+                          left: 50,
+                          right: 50,
+                        ),
                         child: TextFormField(
                           controller: usernameController,
-                          decoration: InputDecoration(
-                            filled: true,
-                            fillColor: Colors.transparent,
-                            labelText: "ناوەکەت داخڵ بکە",
-                            floatingLabelBehavior: FloatingLabelBehavior.auto,
-                            enabledBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Color(0xFF119C59),
-                              ),
-                            ),
-                            labelStyle: TextStyle(color: Color(0xFF52575C)),
-                            errorText: usernameErrorText,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'ناوی بەکارهێنەر',
                           ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'تکایە ناوی بەکارهێنەر بنووسە';
+                            }
+                            return null;
+                          },
                         ),
                       ),
-                    ),
-                    // email
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20, right: 30),
-                      child: SizedBox(
-                        width: 272,
-                        height: 55,
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          top: 20,
+                          left: 50,
+                          right: 50,
+                        ),
                         child: TextFormField(
                           controller: emailController,
-                          decoration: InputDecoration(
-                            filled: true,
-                            fillColor: Colors.transparent,
-                            labelText: "ئیمەیڵەکەت داخڵ بکە",
-                            floatingLabelBehavior: FloatingLabelBehavior.auto,
-                            enabledBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Color(0xFF119C59),
-                              ),
-                            ),
-                            labelStyle: TextStyle(color: Color(0xFF52575C)),
-                            errorText: emailErrorText,
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'ئیمەیڵ',
                           ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'تکایە ئیمەیڵەکەت بنووسە';
+                            }
+                            return null;
+                          },
                         ),
                       ),
-                    ),
-                    // password
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20, right: 30),
-                      child: SizedBox(
-                        width: 272,
-                        height: 55,
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          top: 20,
+                          left: 50,
+                          right: 50,
+                        ),
                         child: TextFormField(
                           controller: passwordController,
-                          decoration: InputDecoration(
-                            filled: true,
-                            fillColor: Colors.transparent,
-                            labelText: "پاسۆردەکەت داخڵ بکە",
-                            floatingLabelBehavior: FloatingLabelBehavior.auto,
-                            enabledBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Color(0xFF119C59),
-                              ),
-                            ),
-                            labelStyle: TextStyle(color: Color(0xFF52575C)),
-                            errorText: passwordErrorText,
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'وشەی نهێنی',
                           ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'تکایە وشەی نهێنی بنووسە';
+                            }
+                            return null;
+                          },
                         ),
                       ),
-                    ),
-                    // confirm password
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20, right: 30),
-                      child: SizedBox(
-                        width: 272,
-                        height: 55,
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          top: 20,
+                          left: 50,
+                          right: 50,
+                        ),
                         child: TextFormField(
-                          decoration: InputDecoration(
-                            labelText: "دووبارە پاسۆردەکەت داخڵ بکە",
-                            filled: true,
-                            fillColor: Colors.transparent,
-                            floatingLabelBehavior: FloatingLabelBehavior.auto,
-                            enabledBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Color(0xFF119C59),
-                              ),
+                          controller: confirmPasswordController,
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'دڵنیابکەرەوەی وشەی نهێنی',
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'تکایە دڵنیابکەرەوەی وشەی نهێنی بنووسە';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFF119C59)),
+                        onPressed: signUp,
+                        child: const Text('خۆتۆمارکردن'),
+                      ),
+                      const SizedBox(height: 20),
+                      const SizedBox(height: 40),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const Login_page(),
                             ),
-                            labelStyle: TextStyle(color: Color(0xFF52575C)),
+                          );
+                        },
+                        child: const Text(
+                          "چوونەژورەوە",
+                          style: TextStyle(
+                            color: Color(0xFF119C59),
+                            fontSize: 23,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
                       ),
-                    ),
-                    // login button
-                    Padding(
-                      padding: const EdgeInsets.only(top: 100),
-                      child: Center(
-                        child: SizedBox(
-                          width: 210,
-                          height: 51,
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(25),
-                              ),
-                              backgroundColor:
-                                  const Color.fromARGB(255, 236, 236, 236),
-                            ),
-                            onPressed: signUp,
-                            child: Padding(
-                              padding: const EdgeInsets.only(top: 9),
-                              child: Column(
-                                children: [
-                                  Center(
-                                    child: Text(
-                                      " دروستکردنی هەژمار",
-                                      style: TextStyle(
-                                        color: Color.fromARGB(255, 0, 0, 0),
-                                        fontSize: 20,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-
-              Padding(
-                padding: const EdgeInsets.only(top: 50),
-                child: Center(
-                  child: InkWell(
-                    onTap: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(builder: (context) => Login_page()),
-                      );
-                    },
-                    child: const Center(
-                      child: Text(
-                        "  چوونەژورەوە    ",
-                        style: TextStyle(
-                          decoration: TextDecoration.underline,
-                          fontSize: 18,
-                          color: Color(0xFF52575C),
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
+                    ],
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
     );
   }
+}
+
+void main() {
+  runApp(const MaterialApp(
+    home: SignUpPage(),
+  ));
 }
